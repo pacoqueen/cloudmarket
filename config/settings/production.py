f193ff24f00f781155b2fff985e22674d14269e7
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2017-02-06 23:41:49 +0100

    boto (S3 AWS)disabled

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,7 @@ Production Configurations
 """
 from __future__ import absolute_import, unicode_literals
 
-from boto.s3.connection import OrdinaryCallingFormat
+# from boto.s3.connection import OrdinaryCallingFormat
 from django.utils import six
 
 import logging
