d552d3efc84c8981d0ab70bb650d3b0288c02747
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2017-02-07 16:17:12 +0100

    Templates for gifts created.

--- gifts/urls.py
+++ gifts/urls.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-from gifts import views
+from . import views
 
+app_name = 'gifts'
 urlpatterns = [# por ejemplo: /gifts/
-               url(r'^$', views.index, name = "index"),
+               url(r'^$', views.IndexView.as_view(), name = "index"),
                # por ejemplo: /gifts/5
-               url(r'^(?P<gift_id>\d+)/$', views.detail, name = "detail"),
+               url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = "detail"),
                # por ejemplo: /gifts/5/mark
-               url(r'^(?P<gift_id>\d+)/mark/$', views.mark, name = "mark"),
+               url(r'^(?P<gift_id>[0-9]+)/mark/$', views.mark, name = "mark"),
               ]
--- gifts/views.py
+++ gifts/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-from django.http import HttpResponse
+from .models import Gift
 
-def index(request):
-    return HttpResponse("Hola holita. Gifts index.")
 
-def detail(request, gift_id):
-    return HttpResponse("Detalle del regalo %s." % gift_id)
+class IndexView(generic.ListView):
+    template_name = 'gifts/index.html'
+    context_object_name = 'gift_list'
+
+    def get_queryset(self):
+        """Devuelve todos los regalos de la base de datos."""
+        return Gift.objects.order_by("-date")[:]
+
+
+class DetailView(generic.DetailView):
+    model = Gift
+    template_name = 'gifts/detail.html'
+
 
 def mark(request, gift_id):
-    return HttpResponse("Se ha marcado como hecho el regalo %s." % gift_id)
+    gift = get_object_or_404(Gift, pk=gift_id)
+    done = "done" in request.POST  # Only "on" checkmarks are submitted.
+    gift.done = done
+    gift.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    #return HttpResponseRedirect(reverse('gifts:detail', args=(gift_id, )))
+    return HttpResponseRedirect(reverse('gifts:index'))
