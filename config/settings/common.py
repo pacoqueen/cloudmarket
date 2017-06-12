2cee409e33675f4682e7d22c80d4ea52f4894c44
Francisco José Rodríguez Bogado <bogado@qinn.es>
2017-06-12 20:44:56 +0200

    Changed default admin url.

--- config/settings/common.py
+++ config/settings/common.py
@@ -251,7 +251,7 @@ AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 
 
 # Location of root django.contrib.admin URL, use {% url 'admin:index' %}
-ADMIN_URL = r'^admin/'
+ADMIN_URL = r'^adminpanel/'
 
 # Your common stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
