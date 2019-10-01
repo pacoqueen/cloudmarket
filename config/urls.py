65a88250ae3865882910e6f8a53dbb21415d8a41
Francisco José Rodríguez Bogado <bogado@qinn.es>
2019-10-01 12:48:15 +0200

    Fixes https://sentry.io/organizations/q-inn/issues/1250715240/events/7cf0839541b1485fb7bd0562aee6257d/

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
+app_name = 'cloudmarket'
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
