fa1d9e6c1410896fd5f4b0b5380ea4dad6c685c8
Paco Queen <pacoqueen@gmail.com>
2022-08-29 10:12:12 +0200

    Setting up new test environment.

--- config/settings/local.py
+++ config/settings/local.py
@@ -72,3 +72,4 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
 # Your local stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['cloudmarket.es'])
